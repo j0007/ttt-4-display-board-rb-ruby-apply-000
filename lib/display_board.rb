5b99b274355752928905439b3fb440b4d001e62a
John Chen <chenj317@gmail.com>
2020-07-14 17:31:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 board = [""," "," "," "," "," "," "," "," "]
 
 def display_board(xo = "\n")
-  puts " #{xo[0]} | #{xo[1]} | #{xo[3]} "
+  puts " #{xo[0]} | #{xo[1]} | #{xo[3]}  "
   puts "-----------"
-  puts " #{xo[4]} | #{xo[5]} | #{xo[6]} "
+  puts " #{xo[4]} | #{xo[5]} | #{xo[6]}  "
   puts "-----------"
-  puts " #{xo[7]} | #{xo[8]} | #{xo[9]} "
+  puts " #{xo[7]} | #{xo[8]} | #{xo[9]}  "
 end
 
 display_board(board)
